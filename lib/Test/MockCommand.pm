c2dcf34bf7803284327c4602e39258be198790e0
Stuart Caie <kyzer@4u.net>
2012-04-20 09:41:34 +0100

    bump to version 0.3

--- lib/Test/MockCommand.pm
+++ lib/Test/MockCommand.pm
@@ -11,7 +11,7 @@ use Test::MockCommand::Recorder;
 use Test::MockCommand::Result;
 use Test::MockCommand::ScalarReadline qw(scalar_readline);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our $CLASS = __PACKAGE__;
 our $OPEN_HANDLER = undef; # this gets set to _default_open_handler
 our $RECORDING = 0;        # are we recording commands or playing them back?
@@ -572,7 +572,7 @@ Stuart Caie, E<lt>kyzer@4u.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2009 by Stuart Caie
+Copyright (C) 2008-2012 by Stuart Caie
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
